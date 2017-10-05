2f8f22aa943b0146d9b288340472c0a154419c2f
inferman <infernal.manowar@gmail.com>
2017-10-06 00:17:09 +0300

    added slider styles and footer fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,26 @@
 $(document).foundation();
+$(document).ready(function(){
+    $('.news_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
