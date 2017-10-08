5ff9d6d1a90fa7c20e4aa46cd259957d1d8eac8e
inferman <infernal.manowar@gmail.com>
2017-10-08 19:57:06 +0300

    completed product slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
         autoplaySpeed: 3000,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 1,
@@ -40,10 +40,11 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
                 }
             }
         ],
